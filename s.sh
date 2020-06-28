80e8ed7b0e4c6cef0eeaf88c5d8b815bb71cf496
Naufal Prima Yoriko <48062674+primayoriko@users.noreply.github.com>
2020-06-28 19:01:28 +0700

    Delete s.sh

--- s.sh
+++
@@ -1,4 +0,0 @@
-# nasm -felf32 game.asm && ld -melf_i386 -o game game.o -e main
-# gcc  -fno-pie -fno-asynchronous-unwind-tables -O2 -s -c -o game.o game.c
-gcc  -fno-asynchronous-unwind-tables -O2 -s -c -o game.o game.c
-./objconv -fnasm game.o
